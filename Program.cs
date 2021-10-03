b1a9af3641543b10e7908be88dc03c56649c8a1e
harfouche <gabriel.harfouche@lyceefulbert.fr>
2021-10-03 15:45:29 +0200

    Test 1

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace Autofact
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //TEST
         }
     }
 }
