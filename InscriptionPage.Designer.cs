96843f1fe0c61f10d9b88751c098d17e01263e80
harfouche <gabriel.harfouche@lyceefulbert.fr>
2021-10-04 12:29:32 +0200

    Inscription Insert

---
+++ InscriptionPage.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Autofact
+{
+    partial class InscriptionPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.box_mail = new System.Windows.Forms.TextBox();
+            this.box_mdp = new System.Windows.Forms.TextBox();
+            this.btn_inscription = new System.Windows.Forms.Button();
+            this.box_nom = new System.Windows.Forms.TextBox();
+            this.box_prenom = new System.Windows.Forms.TextBox();
+            this.box_confmdp = new System.Windows.Forms.TextBox();
+            this.label_email = new System.Windows.Forms.Label();
+            this.label_nom = new System.Windows.Forms.Label();
+            this.label_prenom = new System.Windows.Forms.Label();
+            this.label_mdp = new System.Windows.Forms.Label();
+            this.label_confmdp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inscription";
+            // 
+            // box_mail
+            // 
+            this.box_mail.Location = new System.Drawing.Point(361, 106);
+            this.box_mail.Name = "box_mail";
+            this.box_mail.Size = new System.Drawing.Size(161, 22);
+            this.box_mail.TabIndex = 1;
+            this.box_mail.TextChanged += new System.EventHandler(this.box_mail_TextChanged);
+            // 
+            // box_mdp
+            // 
+            this.box_mdp.Location = new System.Drawing.Point(360, 273);
+            this.box_mdp.Name = "box_mdp";
+            this.box_mdp.Size = new System.Drawing.Size(161, 22);
+            this.box_mdp.TabIndex = 2;
+            this.box_mdp.TextChanged += new System.EventHandler(this.box_mdp_TextChanged);
+            // 
+            // btn_inscription
+            // 
+            this.btn_inscription.Location = new System.Drawing.Point(383, 368);
+            this.btn_inscription.Name = "btn_inscription";
+            this.btn_inscription.Size = new System.Drawing.Size(138, 26);
+            this.btn_inscription.TabIndex = 3;
+            this.btn_inscription.Text = "S\'inscrire";
+            this.btn_inscription.UseVisualStyleBackColor = true;
+            this.btn_inscription.Click += new System.EventHandler(this.btn_inscription_Click);
+            // 
+            // box_nom
+            // 
+            this.box_nom.Location = new System.Drawing.Point(361, 162);
+            this.box_nom.Name = "box_nom";
+            this.box_nom.Size = new System.Drawing.Size(161, 22);
+            this.box_nom.TabIndex = 4;
+            this.box_nom.TextChanged += new System.EventHandler(this.box_nom_TextChanged);
+            // 
+            // box_prenom
+            // 
+            this.box_prenom.Location = new System.Drawing.Point(362, 220);
+            this.box_prenom.Name = "box_prenom";
+            this.box_prenom.Size = new System.Drawing.Size(160, 22);
+            this.box_prenom.TabIndex = 5;
+            this.box_prenom.TextChanged += new System.EventHandler(this.box_prenom_TextChanged);
+            // 
+            // box_confmdp
+            // 
+            this.box_confmdp.Location = new System.Drawing.Point(360, 320);
+            this.box_confmdp.Name = "box_confmdp";
+            this.box_confmdp.Size = new System.Drawing.Size(161, 22);
+            this.box_confmdp.TabIndex = 6;
+            this.box_confmdp.TextChanged += new System.EventHandler(this.box_confmdp_TextChanged);
+            // 
+            // label_email
+            // 
+            this.label_email.AutoSize = true;
+            this.label_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_email.Location = new System.Drawing.Point(251, 111);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(51, 20);
+            this.label_email.TabIndex = 7;
+            this.label_email.Text = "Email";
+            // 
+            // label_nom
+            // 
+            this.label_nom.AutoSize = true;
+            this.label_nom.Location = new System.Drawing.Point(251, 167);
+            this.label_nom.Name = "label_nom";
+            this.label_nom.Size = new System.Drawing.Size(37, 17);
+            this.label_nom.TabIndex = 8;
+            this.label_nom.Text = "Nom";
+            // 
+            // label_prenom
+            // 
+            this.label_prenom.AutoSize = true;
+            this.label_prenom.Location = new System.Drawing.Point(251, 225);
+            this.label_prenom.Name = "label_prenom";
+            this.label_prenom.Size = new System.Drawing.Size(57, 17);
+            this.label_prenom.TabIndex = 9;
+            this.label_prenom.Text = "Prénom";
+            // 
+            // label_mdp
+            // 
+            this.label_mdp.AutoSize = true;
+            this.label_mdp.Location = new System.Drawing.Point(251, 278);
+            this.label_mdp.Name = "label_mdp";
+            this.label_mdp.Size = new System.Drawing.Size(93, 17);
+            this.label_mdp.TabIndex = 10;
+            this.label_mdp.Text = "Mot de passe";
+            // 
+            // label_confmdp
+            // 
+            this.label_confmdp.AutoSize = true;
+            this.label_confmdp.Location = new System.Drawing.Point(148, 325);
+            this.label_confmdp.Name = "label_confmdp";
+            this.label_confmdp.Size = new System.Drawing.Size(196, 17);
+            this.label_confmdp.TabIndex = 11;
+            this.label_confmdp.Text = "Confirmation de Mot de passe";
+            // 
+            // InscriptionPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 507);
+            this.Controls.Add(this.label_confmdp);
+            this.Controls.Add(this.label_mdp);
+            this.Controls.Add(this.label_prenom);
+            this.Controls.Add(this.label_nom);
+            this.Controls.Add(this.label_email);
+            this.Controls.Add(this.box_confmdp);
+            this.Controls.Add(this.box_prenom);
+            this.Controls.Add(this.box_nom);
+            this.Controls.Add(this.btn_inscription);
+            this.Controls.Add(this.box_mdp);
+            this.Controls.Add(this.box_mail);
+            this.Controls.Add(this.label1);
+            this.Name = "InscriptionPage";
+            this.Text = "ConnexionPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox box_mail;
+        private System.Windows.Forms.TextBox box_mdp;
+        private System.Windows.Forms.Button btn_inscription;
+        private System.Windows.Forms.TextBox box_nom;
+        private System.Windows.Forms.TextBox box_prenom;
+        private System.Windows.Forms.TextBox box_confmdp;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.Label label_nom;
+        private System.Windows.Forms.Label label_prenom;
+        private System.Windows.Forms.Label label_mdp;
+        private System.Windows.Forms.Label label_confmdp;
+    }
+}
---
+++ InscriptionPage.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace Autofact
+{
+    public partial class InscriptionPage : Form
+    {
+
+        public InscriptionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_inscription_Click(object sender, EventArgs e)
+        {
+            string connectionString = "SERVER=localhost; DATABASE=solucedevautofact; UID=root; PASSWORD=''; SSL MODE='none'";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            
+
+            string insert = "INSERT INTO `utilisateur`(`NOM`, `PRENOM`, `MAIL`, `MDP`) VALUES (@nom,@prenom,@email,@mdp)";
+            MySqlCommand cmd = new MySqlCommand(insert, conn);
+
+            cmd.Parameters.AddWithValue("@nom", box_nom.Text);
+            cmd.Parameters.AddWithValue("@prenom", box_prenom.Text);
+            cmd.Parameters.AddWithValue("@email", box_mail.Text);
+            cmd.Parameters.AddWithValue("@mdp", box_mdp.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void box_nom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box_mail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box_prenom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box_mdp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void box_confmdp_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
